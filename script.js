97df54da269e9b764b22be3d5e70a864e7d59094
Kann Jittiwan <kjittiwan2@gmail.com>
2023-04-10 20:52:43 +0700

    initial

---
+++ script.js
@@ -0,0 +1,45 @@
+const errorMessage = document.querySelector('.error-message')
+const inputForm = document.querySelector('form');
+const textInput = document.querySelector('input[type="text"]');
+const getWeather = async(location) => {
+    errorMessage.style.display = 'none';
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=abfb91983a564e90b5274228231004&q=${location}`, {mode: 'cors'});
+    const weatherData = await response.json();
+    const processedData = processData(weatherData);
+    console.log(weatherData);
+    displayData(processedData);
+}
+
+const processData = (initialData) => {
+  const processedData = {
+    city: initialData.location.name,
+    condition: initialData.current.condition.text,
+    temp: initialData.current.temp_c,
+    feelsLike: initialData.current.feelslike_c,
+    humidity: initialData.current.humidity,
+    wind: initialData.current.wind_kph   
+  }
+  if (initialData.location.country === 'United States of America'){
+    processedData['region'] = initialData.location.region;
+  }
+  else {
+    processedData['region'] = initialData.location.country;
+  }
+  return processedData;
+}
+const displayData = (data) => {
+  document.querySelector('.condition-text').textContent = data.condition
+  document.querySelector('.location-text').textContent = `${data.city}, ${data.region}`
+  document.querySelector('.temp-text').textContent = `${data.temp}°C`
+  document.querySelector('.feelslike-text').textContent = `Feels like: ${data.feelsLike}°C`
+  document.querySelector('.wind-text').textContent = `Wind: ${data.wind} MPH`
+  document.querySelector('.humidity-text').textContent = `Humidity: ${data.humidity}%`
+}
+
+inputForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  getWeather(textInput.value).catch(err => {
+    errorMessage.style.display = 'block';
+  });
+  inputForm.reset();
+})
